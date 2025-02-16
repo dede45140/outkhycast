15dad6e0a3d496834a7eefac418019aa4de498df
oums45 <116953534+dede45140@users.noreply.github.com>
2025-02-16 16:46:14 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,50 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyAM-ob_pjFScXWk7UojJ-Wph5g0O7bjiIs",
+    authDomain: "everyday-e4be4.firebaseapp.com",
+    databaseURL: "https://everyday-e4be4-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "everyday-e4be4",
+    storageBucket: "everyday-e4be4.firebasestorage.app",
+    messagingSenderId: "108146260282",
+    appId: "1:108146260282:web:ae81dfde5ec75fea967803",
+    measurementId: "G-7081N2YTD7"
+  };
+
+  const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+// Sign in function
+document.getElementById('signInBtn').addEventListener('click', () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            console.log(result.user);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+});
+
+// Sign out function
+document.getElementById('signOutBtn').addEventListener('click', () => {
+    signOut(auth).then(() => {
+        console.log('User signed out');
+    }).catch((error) => {
+        console.error(error);
+    });
+});
+
+// Listen to authentication state changes
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        document.getElementById('whenSignedIn').hidden = false;
+        document.getElementById('whenSignedOut').hidden = true;
+        document.getElementById('userDetails').textContent = `Hello, ${user.displayName}. Your UID is: ${user.uid}`;
+    } else {
+        document.getElementById('whenSignedIn').hidden = true;
+        document.getElementById('whenSignedOut').hidden = false;
+    }
+})
+
